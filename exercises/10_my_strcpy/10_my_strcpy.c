c6842534667d7812d72ccc8fccf71cb663654235
lemonsuqing <1428828189@qq.com>
2026-04-13 17:53:01 +0800

    test10 ok

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -4,9 +4,12 @@ int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
     
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
+	int i;
+    for(i = 0; source[i] != '\0'; i++){
+        destination[i] = source[i];
+    }
+    destination[i] = '\0';
+
     printf("拷贝后的字符串: %s", destination);
     
     return 0;
