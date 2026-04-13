9a35c7ee8aa5a9407099b9122b3982018b684b8f
lemonsuqing <1428828189@qq.com>
2026-04-13 14:47:49 +0800

    test08 ok

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -9,20 +9,54 @@ typedef struct {
 
 typedef struct {
     People data[MAX_PEOPLE];
-    int head;
-    int tail;
-    int count;
+    int head;   // 队头：出队位置
+    int tail;   // 队尾：入队位置
+    int count;  // 当前元素个数
 } Queue;
+
+void queue_init(Queue *q) {
+    q->head = 0;
+    q->tail = 0;
+    q->count = 0;
+}
+
+void queue_push(Queue *q, People p) {
+    q->data[q->tail] = p;
+    q->tail = (q->tail + 1) % MAX_PEOPLE;
+    q->count++;
+}
+
+People queue_pop(Queue *q) {
+    People p = q->data[q->head];
+    q->head = (q->head + 1) % MAX_PEOPLE;
+    q->count--;
+    return p;
+}
 
 int main() {
     Queue q;
-    int total_people=50;
-    int report_interval=5;
+    int total_people = 50;
+    int report_interval = 5;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
-    printf("最后剩下的人是: %d\n", q.data[q.head].id);
+    queue_init(&q);
+
+    for(int i = 1; i <= total_people; i ++){
+        queue_push(&q, (People){i});
+    }
+
+    int count = 1;
+    while(q.count > 1){
+        People p = queue_pop(&q);
+        if(count == report_interval){
+            printf("淘汰: %d\n", p.id);
+            count = 1;
+        }else {
+            queue_push(&q, p);
+            count++;
+        }
+    }
+    People last = queue_pop(&q);
+    printf("最后剩下的人是: %d\n", last.id);
 
     return 0;
 }
