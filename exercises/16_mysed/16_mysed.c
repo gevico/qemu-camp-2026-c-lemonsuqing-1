47ccd98f33a14d687e40db433e22b362a32907c7
lemonsuqing <1428828189@qq.com>
2026-04-14 23:56:33 +0800

    test16 ok

--- exercises/16_mysed/16_mysed.c
+++ exercises/16_mysed/16_mysed.c
@@ -8,16 +8,41 @@ int parse_replace_command(const char* cmd, char** old_str, char** new_str) {
     if (cmd[0] != 's' || cmd[1] != '/') {
         return -1;
     }
+    // char old[MAX_LINE_LENGTH], new[MAX_LINE_LENGTH];
+    int i = 2;
+    int start = i;
+    while (cmd[i] != '/' && cmd[i] != '\0') i++;
+    if (cmd[i] != '/') return -1;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    *old_str = malloc(i - start + 1);
+    strncpy(*old_str, cmd + start, i - start);
+    (*old_str)[i - start] = '\0';
+
+    i++;  // 跳过 '/'
+    start = i;
+    
+    // 找 new_str 的结束位置
+    while (cmd[i] != '/' && cmd[i] != '\0') i++;
+    if (cmd[i] != '/') return -1;
+    
+    *new_str = malloc(i - start + 1);
+    strncpy(*new_str, cmd + start, i - start);
+    (*new_str)[i - start] = '\0';
 
     return 0;
 }
 
 void replace_first_occurrence(char* str, const char* old, const char* new) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char* pos = strstr(str, old);
+    if (!pos) return;
+
+    size_t old_len = strlen(old);
+    size_t new_len = strlen(new);
+    size_t tail_len = strlen(pos + old_len);
+
+    memmove(pos + new_len, pos + old_len, tail_len + 1);
+
+    memcpy(pos, new, new_len);
 }
 
 int main(int argc, char* argv[]) {
