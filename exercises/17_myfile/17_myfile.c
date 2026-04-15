816d41ad232cf58deb729f9cb2244d5382b9bcb9
lemonsuqing <1428828189@qq.com>
2026-04-15 11:03:39 +0800

    test17 ok

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -8,8 +8,10 @@
 void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    case ET_REL: type_str = "Relocatable"; break;
+    case ET_EXEC: type_str = "Executable"; break;
+    case ET_DYN: type_str = "Shared Object/PIE"; break;
+    default: type_str = "Unknown"; break;
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -20,11 +22,32 @@ int main(int argc, char *argv[]) {
     "./17_myfile",
   };
 
-  int fd;
-  Elf64_Ehdr ehdr;
+  for(int i = 0; i < 2; i++){
+    int fd = open(filepath[i], O_RDONLY);
+    if(fd < 0){
+      perror("open");
+      continue;
+    }
 
-  // TODO: 在这里添加你的代码
-  // I AM NOT DONE
+    Elf64_Ehdr ehdr;
+    if(read(fd, &ehdr, sizeof(ehdr)) != sizeof(ehdr)){
+      printf("读取 %s 失败\n", filepath[i]);
+      close(fd);
+      continue;
+    }
+
+    if (ehdr.e_ident[0] != 0x7f ||
+        ehdr.e_ident[1] != 'E' ||
+        ehdr.e_ident[2] != 'L' ||
+        ehdr.e_ident[3] != 'F') {
+        printf("%s 不是 ELF 文件\n", filepath[i]);
+        close(fd);
+        continue;
+    }
+
+    print_elf_type(ehdr.e_type);
+    close(fd);
+  }
   
   return 0;
 }
